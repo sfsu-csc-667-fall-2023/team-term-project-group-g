de5a022662d8cffb009ccde66a204dcc20eaf378
itsmeAlec <alecnagal@gmail.com>
2023-12-10 08:58:35 -0800

    Revert "css done"
    This reverts commit 8833899a5b000a7d813c808065e771ef2b1bb3e5.

--- battleship/app.js
+++ battleship/app.js
@@ -20,193 +20,189 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameOver = false
     let currentPlayer = 'user'
     const width = 10
-    let playerNum = 0
-    let ready = false
-    let enemyReady = false
-    let allShipsPlaced = false
-    let shotFired = -1
-    //Ships
-    const shipArray = [
-      {
-        name: 'destroyer',
-        directions: [
-          [0, 1],
-          [0, width]
-        ]
-      },
-      {
-        name: 'submarine',
-        directions: [
-          [0, 1, 2],
-          [0, width, width*2]
-        ]
-      },
-      {
-        name: 'cruiser',
-        directions: [
-          [0, 1, 2],
-          [0, width, width*2]
-        ]
-      },
-      {
-        name: 'battleship',
-        directions: [
-          [0, 1, 2, 3],
-          [0, width, width*2, width*3]
-        ]
-      },
-      {
-        name: 'carrier',
-        directions: [
-          [0, 1, 2, 3, 4],
-          [0, width, width*2, width*3, width*4]
-        ]
-      },
-    ]
-  
-    createBoard(userGrid, userSquares)
-    createBoard(computerGrid, computerSquares)
-  
-    // Select Player Mode
-    if (gameMode === 'singlePlayer') {
-      startSinglePlayer()
+    let gameMode = ""
+let playerNum = 0
+let ready = false
+let enemyReady = false
+let allShipsPlaced = false
+let shotFired = -1
+
+
+// Select Player Mode
+singlePlayerButton.addEventListener('click', startSinglePlayer)
+multiPlayerButton.addEventListener('click', startMultiPlayer)
+
+// Multiplayer
+function startMultiPlayer() {
+    gameMode = 'multiPlayer'
+
+    const socket = io();
+
+// Get your player number
+socket.on('player-number', num => {
+    if (num === -1) {
+        infoDisplay.innerHTML = "Sorry, the server is full"
     } else {
-      startMultiPlayer()
+        playerNum = parseInt(num)
+        if(playerNum === 1) currentPlayer = "enemy"
+
+        console.log(playerNum)
+
+        // Get other player status
+        socket.emit('check-players')
     }
-  
-    // Multiplayer
-    function startMultiPlayer() {
-      const socket = io();
-  
-      // Get your player number
-      socket.on('player-number', num => {
-        if (num === -1) {
-          infoDisplay.innerHTML = "Sorry, the server is full"
-        } else {
-          playerNum = parseInt(num)
-          if(playerNum === 1) currentPlayer = "enemy"
-  
-          console.log(playerNum)
-  
-          // Get other player status
-          socket.emit('check-players')
+})
+
+// Another player has connected or disconnected
+socket.on('player-connection', num => {
+    console.log(`Player number ${num} has connected or disconnected`)
+    playerConnectedOrDisconnected(num)
+})
+
+// On enemy ready
+socket.on('enemy-ready', num => {
+    enemyReady = true
+    playerReady(num)
+    if (ready) playGameMulti(socket)
+})
+
+// Check player status
+socket.on('check-players', players => {
+    players.forEach((p, i) => {
+        if(p.connected) playerConnectedOrDisconnected(i)
+        if(p.ready) {
+        playerReady(i)
+        if(i !== playerReady) enemyReady = true
         }
-      })
-  
-      // Another player has connected or disconnected
-      socket.on('player-connection', num => {
-        console.log(`Player number ${num} has connected or disconnected`)
-        playerConnectedOrDisconnected(num)
-      })
-  
-      // On enemy ready
-      socket.on('enemy-ready', num => {
-        enemyReady = true
-        playerReady(num)
-        if (ready) {
-          playGameMulti(socket)
-          setupButtons.style.display = 'none'
+    })
+})
+
+// On Timeout
+socket.on('timeout', () => {
+    infoDisplay.innerHTML = 'You have reached the 10 minute limit'
+})
+
+// Ready button click
+startButton.addEventListener('click', () => {
+    if(allShipsPlaced) playGameMulti(socket)
+    else infoDisplay.innerHTML = "Please place all ships"
+})
+
+// Setup event listeners for firing
+computerSquares.forEach(square => {
+    square.addEventListener('click', () => {
+        if(currentPlayer === 'user' && ready && enemyReady) {
+        shotFired = square.dataset.id
+        socket.emit('fire', shotFired)
         }
-      })
-  
-      // Check player status
-      socket.on('check-players', players => {
-        players.forEach((p, i) => {
-          if(p.connected) playerConnectedOrDisconnected(i)
-          if(p.ready) {
-            playerReady(i)
-            if(i !== playerReady) enemyReady = true
-          }
-        })
-      })
-  
-      // On Timeout
-      socket.on('timeout', () => {
-        infoDisplay.innerHTML = 'You have reached the 10 minute limit'
-      })
-  
-      // Ready button click
-      startButton.addEventListener('click', () => {
-        if(allShipsPlaced) playGameMulti(socket)
-        else infoDisplay.innerHTML = "Please place all ships"
-      })
-  
-      // Setup event listeners for firing
-      computerSquares.forEach(square => {
-        square.addEventListener('click', () => {
-          if(currentPlayer === 'user' && ready && enemyReady) {
-            shotFired = square.dataset.id
-            socket.emit('fire', shotFired)
-          }
-        })
-      })
-  
-      // On Fire Received
-      socket.on('fire', id => {
-        enemyGo(id)
-        const square = userSquares[id]
-        socket.emit('fire-reply', square.classList)
-        playGameMulti(socket)
-      })
-  
-      // On Fire Reply Received
-      socket.on('fire-reply', classList => {
-        revealSquare(classList)
-        playGameMulti(socket)
-      })
-  
-      function playerConnectedOrDisconnected(num) {
-        let player = `.p${parseInt(num) + 1}`
-        document.querySelector(`${player} .connected`).classList.toggle('active')
-        if(parseInt(num) === playerNum) document.querySelector(player).style.fontWeight = 'bold'
-      }
+    })
+    })
+
+    // On Fire Received
+    socket.on('fire', id => {
+      enemyGo(id)
+      const square = userSquares[id]
+      socket.emit('fire-reply', square.classList)
+      playGameMulti(socket)
+    })
+
+// On Fire Reply Received
+socket.on('fire-reply', classList => {
+    revealSquare(classList)
+    playGameMulti(socket)
+})
+
+function playerConnectedOrDisconnected(num) {
+    let player = `.p${parseInt(num) + 1}`
+    document.querySelector(`${player} .connected span`).classList.toggle('green')
+    if(parseInt(num) === playerNum) document.querySelector(player).style.fontWeight = 'bold'
     }
-  
-    // Single Player
-    function startSinglePlayer() {
-      generate(shipArray[0])
-      generate(shipArray[1])
-      generate(shipArray[2])
-      generate(shipArray[3])
-      generate(shipArray[4])
-  
-      startButton.addEventListener('click', () => {
-        setupButtons.style.display = 'none'
-        playGameSingle()
-      })
+}
+
+// Single Player
+function startSinglePlayer() {
+    gameMode = "singlePlayer"
+
+    generate(shipArray[0])
+    generate(shipArray[1])
+    generate(shipArray[2])
+    generate(shipArray[3])
+    generate(shipArray[4])
+
+    startButton.addEventListener('click', playGameSingle)
+}
+
+//Create Board
+function createBoard(grid, squares) {
+    for (let i = 0; i < width*width; i++) {
+    const square = document.createElement('div')
+    square.dataset.id = i
+    grid.appendChild(square)
+    squares.push(square)
     }
-  
-    //Create Board
-    function createBoard(grid, squares) {
-      for (let i = 0; i < width*width; i++) {
-        const square = document.createElement('div')
-        square.dataset.id = i
-        grid.appendChild(square)
-        squares.push(square)
-      }
-    }
-  
-    //Draw the computers ships in random locations
-    function generate(ship) {
-      let randomDirection = Math.floor(Math.random() * ship.directions.length)
-      let current = ship.directions[randomDirection]
-      if (randomDirection === 0) direction = 1
-      if (randomDirection === 1) direction = 10
-      let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
-  
-      const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
-      const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
-      const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
-  
-      if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
-  
-      else generate(ship)
-    }
-    
-  
-    //Rotate the ships
-    function rotate() {
-      if (isHorizontal) {
+}
+createBoard(userGrid, userSquares)
+createBoard(computerGrid, computerSquares)
+
+//Ships
+const shipArray = [
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width]
+      ]
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width*2, width*3]
+      ]
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width*2, width*3, width*4]
+      ]
+    },
+]
+
+//Draw the computers ships in random locations
+function generate(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length)
+    let current = ship.directions[randomDirection]
+    if (randomDirection === 0) direction = 1
+    if (randomDirection === 1) direction = 10
+    let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
+
+    const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+    const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
+    const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+
+    if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+
+    else generate(ship)
+}
+
+//Rotate the ships
+function rotate() {
+    if (isHorizontal) {
         destroyer.classList.toggle('destroyer-container-vertical')
         submarine.classList.toggle('submarine-container-vertical')
         cruiser.classList.toggle('cruiser-container-vertical')
