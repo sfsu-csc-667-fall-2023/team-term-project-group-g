dcf35bea8e8457157ea49f6afbbae12d3570e99e
Vijayt2001 <62590408+Vijayt2001@users.noreply.github.com>
2023-12-15 19:52:18 -0800

    socket issue for game

--- server.js
+++ server.js
@@ -1,45 +1,51 @@
-const express = require('express');
+const express = require("express");
 const { pool } = require("./dbConfig");
-const path = require('path');
-const passport = require('passport');
-const socketio = require('socket.io');
+const path = require("path");
+const passport = require("passport");
+const socketio = require("socket.io");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const io = socketio(server);
-const session = require('express-session');
-const MemoryStore = require('session-memory-store')(session);
+const session = require("express-session");
+const MemoryStore = require("session-memory-store")(session);
 const bcrypt = require("bcrypt");
 const PORT = process.env.PORT || 4000;
 const initializePassport = require("./passportConfig");
-const flash = require('express-flash');
-
+const flash = require("express-flash");
 
 initializePassport(passport);
 
-const sessionMiddleware = session({
+const socketSession = session({
   store: new MemoryStore(),
-  secret: 'Alphabet',
+  secret: "SocketSecret",
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
 });
 
+const sessionMiddleware = session({
+  store: new MemoryStore(),
+  secret: "Alphabet",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+});
+
+app.use(sessionMiddleware);
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,'views')));
-
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
-// Funtion inside passport which initializes passport
+
 app.use(passport.initialize());
-// Store our variables to be persisted across the whole session. Works with app.use(Session) above
 app.use(passport.session());
 app.use(flash());
 
@@ -70,8 +76,13 @@ app.get("/users/landingPage", checkNotAuthenticated, (req, res) => {
 });
 
 app.get("/users/logout", (req, res) => {
-  req.logout();
-  res.render("index", { message: "You have logged out successfully" });
+  req.logout((err) => {
+    if (err) {
+      console.error(err);
+      return res.render("error", { error: "Logout failed" });
+    }
+    res.redirect("http://localhost:4000");
+  });
 });
 
 app.post("/users/register", async (req, res) => {
@@ -83,7 +94,7 @@ app.post("/users/register", async (req, res) => {
     name,
     email,
     password,
-    password2
+    password2,
   });
 
   if (!name || !email || !password || !password2) {
@@ -91,7 +102,7 @@ app.post("/users/register", async (req, res) => {
   }
 
   if (password.length < 6) {
-    errors.push({ message: "Password must be a least 6 characters long" });
+    errors.push({ message: "Password must be at least 6 characters long" });
   }
 
   if (password !== password2) {
@@ -116,7 +127,7 @@ app.post("/users/register", async (req, res) => {
 
         if (results.rows.length > 0) {
           return res.render("register", {
-            message: "Email already registered"
+            message: "Email already registered",
           });
         } else {
           pool.query(
@@ -144,7 +155,7 @@ app.post(
   passport.authenticate("local", {
     successRedirect: "/users/landingPage",
     failureRedirect: "/users/login",
-    failureFlash: true
+    failureFlash: true,
   })
 );
 
@@ -166,11 +177,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use(sessionMiddleware);
-
-
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 const lobbies = {};
 let nextLobbyId = 1; // Start with lobby ID 1
 
@@ -178,17 +184,21 @@ io.use((socket, next) => {
   sessionMiddleware(socket.request, {}, next);
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   let playerIndex = null;
   const session = socket.request.session;
   const lobbyId = session ? session.lobbyId : null;
 
   if (session && lobbyId && lobbies[lobbyId]) {
-    const existingPlayerIndex = lobbies[lobbyId].findIndex((s) => s.request.sessionID === socket.request.sessionID);
+    const existingPlayerIndex = lobbies[lobbyId].findIndex(
+      (s) => s.request.sessionID === socket.request.sessionID
+    );
 
     if (existingPlayerIndex !== -1) {
       // Player is already in the lobby, ignore the connection
-      console.log(`Player ${existingPlayerIndex} reconnected to lobby ${lobbyId}`);
+      console.log(
+        `Player ${existingPlayerIndex} reconnected to lobby ${lobbyId}`
+      );
       playerIndex = existingPlayerIndex;
     } else {
       // The player was not in the lobby, clear the session data
@@ -204,19 +214,19 @@ io.on('connection', (socket) => {
       lobbies[lobbyId][playerIndex] = socket;
       socket.join(lobbyId);
 
-      socket.emit('player-info', { playerIndex, lobbyId });
-      io.to(lobbyId).emit('player-connection', { playerIndex, lobbyId });
+      socket.emit("player-info", { playerIndex, lobbyId });
+      io.to(lobbyId).emit("player-connection", { playerIndex, lobbyId });
 
       console.log(`Player ${playerIndex} reconnected to lobby ${lobbyId}`);
     } else {
       socket.request.session.lobbyId = null;
       socket.request.session.save();
 
-      socket.emit('lobby-full');
+      socket.emit("lobby-full");
     }
   }
 
-  socket.on('create-lobby', () => {
+  socket.on("create-lobby", () => {
     const lobbyId = nextLobbyId++;
     lobbies[lobbyId] = [null, null]; // Initially, no players in the lobby
     socket.join(lobbyId);
@@ -225,109 +235,117 @@ io.on('connection', (socket) => {
     socket.request.session.lobbyId = lobbyId;
     socket.request.session.save();
 
-    socket.emit('created-lobby', { playerIndex: 0, lobbyId });
-    io.emit('lobbies-list', getLobbiesList());
+    socket.emit("created-lobby", { playerIndex: 0, lobbyId });
+    io.emit("lobbies-list", getLobbiesList());
   });
 
-  socket.on('join-lobby', (selectedLobbyId) => {
+  socket.on("join-lobby", (selectedLobbyId) => {
     if (lobbies[selectedLobbyId]) {
       const playerIndex = lobbies[selectedLobbyId].indexOf(null);
-  
+
       if (playerIndex !== -1) {
         lobbies[selectedLobbyId][playerIndex] = socket;
         socket.join(selectedLobbyId);
-  
+
         // Save the lobby ID to the session
         socket.request.session.lobbyId = selectedLobbyId;
         socket.request.session.save();
-  
-        socket.emit('player-info', { playerIndex, lobbyId: selectedLobbyId });
-        io.to(selectedLobbyId).emit('player-connection', { playerIndex, lobbyId: selectedLobbyId });
-  
+
+        socket.emit("player-info", { playerIndex, lobbyId: selectedLobbyId });
+        io.to(selectedLobbyId).emit("player-connection", {
+          playerIndex,
+          lobbyId: selectedLobbyId,
+        });
+
         console.log(`Player ${playerIndex} joined lobby ${selectedLobbyId}`);
-        
+
         // Emit the updated lobbies list to all clients
-        io.emit('lobbies-list', getLobbiesList());
+        io.emit("lobbies-list", getLobbiesList());
       } else {
         // Lobby is full or in an invalid state, clear the session data
         socket.request.session.lobbyId = null;
         socket.request.session.save();
-  
-        socket.emit('lobby-full');
+
+        socket.emit("lobby-full");
       }
     }
   });
-  
-  io.to(lobbyId).emit('player-connection', { playerIndex, lobbyId });
+
+  io.to(lobbyId).emit("player-connection", { playerIndex, lobbyId });
 
   // Handle the request for the list of existing lobbies
-  socket.on('request-lobbies', () => {
+  socket.on("request-lobbies", () => {
     const lobbiesList = getLobbiesList();
-    socket.emit('lobbies-list', lobbiesList);
+    socket.emit("lobbies-list", lobbiesList);
   });
 
   // Handle the 'Create Lobby' event
-  socket.on('create-lobby', () => {
+  socket.on("create-lobby", () => {
     const lobbyId = nextLobbyId++;
     lobbies[lobbyId] = [null, null]; // Assuming the creator joins as the first player
     socket.join(lobbyId);
 
-    socket.emit('created-lobby', { playerIndex: 0, lobbyId });
-    io.emit('lobbies-list', getLobbiesList());
+    socket.emit("created-lobby", { playerIndex: 0, lobbyId });
+    io.emit("lobbies-list", getLobbiesList());
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     // ... existing disconnect logic
     if (playerIndex !== null && lobbies[lobbyId][playerIndex] !== null) {
       console.log(`Player ${playerIndex} in lobby ${lobbyId} disconnected`);
       lobbies[lobbyId][playerIndex] = null;
-      socket.broadcast.emit('player-connection', { playerIndex, lobbyId });
+      socket.broadcast.emit("player-connection", { playerIndex, lobbyId });
     }
   });
-  
-  io.to(lobbyId).emit('player-connection', { playerIndex, lobbyId });
 
-  socket.on('player-connection', ({ playerIndex, lobbyId }) => {
-    io.to(lobbyId).emit('player-connection', { playerIndex, lobbyId });
+  io.to(lobbyId).emit("player-connection", { playerIndex, lobbyId });
+
+  socket.on("player-connection", ({ playerIndex, lobbyId }) => {
+    io.to(lobbyId).emit("player-connection", { playerIndex, lobbyId });
   });
 
-  socket.on('player-ready', () => {
-    socket.to(lobbyId).emit('enemy-ready', playerIndex);
+  socket.on("player-ready", () => {
+    socket.to(lobbyId).emit("enemy-ready", playerIndex);
     lobbies[lobbyId][playerIndex] = true;
 
     // Check if both players are ready
     if (lobbies[lobbyId][0] && lobbies[lobbyId][1]) {
-      io.to(lobbyId).emit('start-game');
+      io.to(lobbyId).emit("start-game");
     }
   });
 
-  socket.on('check-players', () => {
+  socket.on("check-players", () => {
     const players = [];
     for (let i = 0; i < lobbies[lobbyId].length; i++) {
       lobbies[lobbyId][i] === null
         ? players.push({ connected: false, ready: false })
         : players.push({ connected: true, ready: lobbies[lobbyId][i] });
     }
-    socket.emit('check-players', players);
+    socket.emit("check-players", players);
   });
 
-  socket.on('chat-content', (message) => {
-    io.to(lobbyId).emit('chat-content', message);
+  socket.on("chat-content", (message) => {
+    io.to(lobbyId).emit("chat-content", message);
   });
 
-  socket.on('fire', (id) => {
-    console.log(`Shot fired from player ${playerIndex} in lobby ${lobbyId}`, id);
-    socket.to(lobbyId).emit('fire', id);
+  socket.on("fire", (id) => {
+    console.log(
+      `Shot fired from player ${playerIndex} in lobby ${lobbyId}`,
+      id
+    );
+    socket.to(lobbyId).emit("fire", id);
   });
 
-  socket.on('fire-reply', (square) => {
+  socket.on("fire-reply", (square) => {
     console.log(square);
-    socket.to(lobbyId).emit('fire-reply', square);
+    socket.to(lobbyId).emit("fire-reply", square);
   });
-  
+
   function getLobbiesList() {
     return Object.keys(lobbies).map((lobbyId) => {
-      const playersCount = lobbies[lobbyId].filter((player) => player !== null).length;
+      const playersCount = lobbies[lobbyId].filter(
+        (player) => player !== null
+      ).length;
       const maxPlayers = 2; // Change this based on your game's max players
       return { id: lobbyId, players: playersCount, maxPlayers };
     });
@@ -337,7 +355,7 @@ io.on('connection', (socket) => {
     if (lobbies[lobbyId][playerIndex] !== null) {
       console.log(`Player ${playerIndex} in lobby ${lobbyId} timed out`);
       lobbies[lobbyId][playerIndex] = null;
-      socket.emit('timeout');
+      socket.emit("timeout");
       socket.disconnect();
     }
   }, 600000);
